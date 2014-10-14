8672e3689ab680ce576e3f4e67c03a3b9c312261
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 10:23:42 +0900

    > 데이터 입력관련 생성

--- controller/SNSFormLibraryController.js
+++ controller/SNSFormLibraryController.js
@@ -1,3 +1,33 @@
 (function(window, snsBoard){
   'use strict';
+  
+  snsBoard
+    .controller('SNSFormLibraryController', function($scope, snsListStorage){
+      $scope.form = {};
+      
+      $scope.submit = function(data){
+        $scope.form = angular.copy(data);
+        $scope.form.writeName = 'hyyoon';
+        $scope.form.writeImage = 'yellow';
+        $scope.form.writeDate = getTime();
+        snsListStorage.put($scope.form);
+        $scope.form = {};
+      };
+    })
+    .directive('snsForm', function(){
+      return {
+        restrict : 'E',
+        templateUrl : './directive/form.html'
+      };
+    });
+    //polyfill
+    function getTime(){
+      if(!Date.now){
+        Date.now = function now(){
+          return new Date().getTime();
+        };
+      } else {
+        return Date.now();
+      }
+    }
 })(window, snsBoard);
