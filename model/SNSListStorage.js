50bf393bb9b09ab003c9ffd8d07d66c70153aaf9
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 10:21:02 +0900

    > localStorage 를 이용해서 데이터를 저장했던 부분을 array > 형태의 데이터 저장소로 변경

--- model/SNSListStorage.js
+++ model/SNSListStorage.js
@@ -1,17 +1,19 @@
 (function(window, snsBoard){
   'use strict';
   
-  snsBoard.factory('snsListStorage', ['localStorageService', function(storage){
+  snsBoard.factory('snsListStorage', ['Lists', function(storage){
     return {
       get : function(){
-        return storage.get();
+        //return storage.get();
+        return storage;
       },
       put : function(datas){
-        storage.set(datas);
+        //storage.set(datas);
+        storage.push(datas);
       },
       remove : function(){
         
       }
-    }
+    };
   }]);
 })(window, snsBoard);
