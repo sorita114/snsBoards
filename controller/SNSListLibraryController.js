9154e16cf8f666890f476f20981c63705622e17b
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 09:36:00 +0900

    > 임시 데이터 삭제

--- controller/SNSListLibraryController.js
+++ controller/SNSListLibraryController.js
@@ -3,14 +3,14 @@
   
   snsBoard
     .controller('SNSListLibraryController', function($scope, snsListStorage){
-      //$scope.datas = snsListStorage.get();
+      $scope.datas = snsListStorage.get();
       //example
-      $scope.datas = [{
-        writeName : 'hyyoon',
-        writeImage : 'blue',
-        writeDate : '2014-10-13 10:46:00',
-        comment : 'test'
-      }];
+      // $scope.datas = [{
+      //   writeName : 'hyyoon',
+      //   writeImage : 'blue',
+      //   writeDate : '2014-10-13 10:46:00',
+      //   comment : 'test'
+      // }];
     })
     .directive('snsList', function(){
       return{
