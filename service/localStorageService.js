3dca11f47a65fa18a95e3b948280a8987add41b8
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 09:36:45 +0900

    > JSON.parse => angular.fromJson 함수로 변경

--- service/localStorageService.js
+++ service/localStorageService.js
@@ -6,11 +6,11 @@
     
     return {
       get : function(){
-        return JSON.parse(localStorage.getItem(STORAGE_ID) || '{}');
+        return angular.fromJson(localStorage.getItem(STORAGE_ID));
       },
       set : function(datas){
         localStorgae.setItem(STORAGE_ID, JSON.stringify(datas));
       }
-    }
+    };
   });
 })(window, snsBoard);
