c0f1f9497cd35981589bc3ca48992e22d561fcda
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 09:37:59 +0900

    > 임시 데이터 사용 부분 수정 > 데이터가 없을 시 totalItems 에러 수정

--- controller/SNSPagingLibraryController.js
+++ controller/SNSPagingLibraryController.js
@@ -1,13 +1,20 @@
 (function(window, snsBoard){
   'use strict';
   
-  snsBoard.controller('SNSPagingLibraryController', function($scope, snsListStorage){
-    $scope.totalItems = snsListStorage.get().length;
-    $scope.currentPage = 1;
-    $scope.maxSize = 5;
-    
-    $scope.pageChanged = function() {
-      console.log('Page changed to: ' + $scope.currentPage);
-    };
-  });
+  snsBoard
+    .controller('SNSPagingLibraryController', function($scope, snsListStorage){
+      $scope.totalItems = snsListStorage.get() ? snsListStorage.get().length : 0;
+      $scope.currentPage = 1;
+      $scope.maxSize = 5;
+      
+      $scope.pageChanged = function() {
+        console.log('Page changed to: ' + $scope.currentPage);
+      };
+    })
+    .directive('snsPagenation', function(){
+      return{
+        restrict : 'E',
+        templateUrl : './directive/pagenation.html'
+      };
+    });
 })(window, snsBoard);
