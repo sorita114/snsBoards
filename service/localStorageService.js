cc69ea0ad16b139644b99326e2c627f1af49a6aa
mgz708 <mgz708@corp.auction.co.kr>
2014-10-14 09:39:26 +0900

    > json.stringfy 를 angular.toJson 함수로 변경

--- service/localStorageService.js
+++ service/localStorageService.js
@@ -9,7 +9,7 @@
         return angular.fromJson(localStorage.getItem(STORAGE_ID));
       },
       set : function(datas){
-        localStorgae.setItem(STORAGE_ID, JSON.stringify(datas));
+        localStorgae.setItem(STORAGE_ID, angular.toJson(datas));
       }
     };
   });
